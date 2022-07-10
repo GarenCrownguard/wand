6a098cc760b2c9b01beb89aa76000a09e603789a
Mehul Warade <mehulwarade@gmail.com>
2022-07-10 19:45:21 +1000

    icon import fixed

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,19 +1,19 @@
-import iconBurger from "./Icon-burger";
+import iconBurger from "./icon-burger";
 
-import IconLogo from "./Icon-logo";
-import IconSoon from "./Icon-soon";
-import IconWallet from "./Icon-wallet";
-import IconSwap from "./Icon-swap";
-import IconVerticalseperator from "./Icon-verticalseperator";
+import IconLogo from "./icon-logo";
+import IconSoon from "./icon-soon";
+import IconWallet from "./icon-wallet";
+import IconSwap from "./icon-swap";
+import IconVerticalseperator from "./icon-verticalseperator";
 
-import iconAccount from "./Icon-account";
-import iconBridge from "./Icon-bridge";
-import iconDashboard from "./Icon-dashboard";
-import iconDocs from "./Icon-docs";
-import iconGovernance from "./Icon-governance";
-import iconLending from "./Icon-lending";
-import iconSwapSidebar from "./Icon-swap-sidebar";
-import iconVote from "./Icon-vote";
+import iconAccount from "./icon-account";
+import iconBridge from "./icon-bridge";
+import iconDashboard from "./icon-dashboard";
+import iconDocs from "./icon-docs";
+import iconGovernance from "./icon-governance";
+import iconLending from "./icon-lending";
+import iconSwapSidebar from "./icon-swap-sidebar";
+import iconVote from "./icon-vote";
 
 export {
   iconBurger,
