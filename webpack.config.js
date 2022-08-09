9868bc5a115c244b27abdfe44b87cf0137ab245c
GarenCrowngaurd <0xgaren@protonmail.com>
2022-08-09 12:47:29 +1000

    build works

--- webpack.config.js
+++ webpack.config.js
@@ -12,14 +12,6 @@ module.exports = {
       { test: /\.css$/, exclude: /node_modules/, use: "css-loader" },
       { test: /\.json$/, exclude: /node_modules/, use: "json-loader" },
     ],
-    // loaders: {
-    //   test: /\.(js)$/,
-    //   exclude: /(node_modules|bower_components)/,
-    //     loaders: [
-    //         'react-hot',
-    //         'babel?presets[]=react,presets[]=es2015,presets[]=stage-0'
-    //   ]
-    // },
   },
   resolve: {
     extensions: ["*", ".js"],
