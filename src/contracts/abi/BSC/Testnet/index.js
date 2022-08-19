791d84622b91343322f97931134b63f1c5b699fa
GarenCrowngaurd <0xgaren@protonmail.com>
2022-08-19 15:46:41 +1000

    removed extra abis and addresses

--- src/contracts/abi/BSC/Testnet/index.js
+++
@@ -1,6 +0,0 @@
-import mockUSDCabi from './mockUSDC.json'
-import SPTRabi from './SPTR.json'
-import BATONabi from './BATON.json'
-import wandabi from './wand.json'
-
-export { mockUSDCabi as USDCabi, SPTRabi, BATONabi, wandabi }
